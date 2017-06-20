116494c76186e555ba5270113699c00e65ea5b2f
mihirk <mihir.khatwani@gmail.com>
2017-06-20 15:59:43 +0530

    Adding youtube download part

--- bub/app.py
+++ bub/app.py
@@ -1,20 +1,63 @@
+import youtube_dl
 from flask import Flask
-from datetime import datetime
+
+from flask_restful import Api, Resource, reqparse
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        print(msg)
+
+    def warning(self, msg):
+        print(msg)
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+def upload_song(song_file_name):
+    print(song_file_name)
+
+
+def progress_hook(progress):
+    if progress['status'] == 'finished':
+        upload_song(progress['filename'])
+
+
+def download_song(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'progress_hooks': progress_hook,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        return ydl.download([url])
+
 
 app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('url')
 
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+class Song(Resource):
+    def get(self):
+        url = parser.parse_args()['url']
+        download_success = download_song(url)
+        return {'downloaded': bool(download_success)}
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
-
+api.add_resource(Song, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
